267f7d6a5d8f39b2b68bc459bceff35810ee8a73
erickslk <erick.osorto@unitec.edu>
2024-12-10 22:02:24 -0600

    primero comit

---
+++ Components/Navegacion.tsx
@@ -0,0 +1,26 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { NavigationContainer } from '@react-navigation/native'
+
+import Acelerometro from '../Pages/Acelerometro'
+import Logs from '../Pages/Logs'
+
+
+
+export default function NavegacionComponent() {
+
+  const tabNavigation = createBottomTabNavigator()
+  return (
+    
+    <NavigationContainer>
+
+        <tabNavigation.Navigator initialRouteName='Acelerometro'>
+            <tabNavigation.Screen name='Acelerometro' component={Acelerometro}></tabNavigation.Screen>
+            <tabNavigation.Screen name='Logs' component={Logs}></tabNavigation.Screen>
+            
+           
+        </tabNavigation.Navigator>
+    </NavigationContainer>
+  )
+}
